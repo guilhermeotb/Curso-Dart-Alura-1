55bc8a9ea005d1f2717c9bd8a309e001e97aa4d1
Pereira <pereira@eb.mil.br>
2026-02-09 14:34:27 -0300

    #3 Condicionais para operação implementados

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
  double valor1 = double.parse(stdin.readLineSync()!);
  double valor2 = double.parse(stdin.readLineSync()!); 
- 
+ String operacao = stdin.readLineSync()!;
 
  void soma(){
   print( valor1 + valor2);
@@ -21,10 +21,23 @@ void multiplicacao(){
   print( valor1 * valor2);
 }
 
+
+if (operacao == "+"){
  soma();
+}
+
+if (operacao == "-"){
  subtracao();
+}
+ 
+ if (operacao == "/"){
  divisao();
+}
+ 
+ if (operacao == "*"){
  multiplicacao();
+}
+ 
 
 
 }
