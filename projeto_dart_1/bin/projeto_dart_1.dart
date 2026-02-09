2b4051eb77abe2771f37139ab6b60c67eab4d091
Pereira <pereira@eb.mil.br>
2026-02-09 12:06:34 -0300

    #1 Input de idade adicionado

--- projeto_dart_1/bin/projeto_dart_1.dart
+++ projeto_dart_1/bin/projeto_dart_1.dart
@@ -2,6 +2,10 @@ import 'dart:io';
 
 void main() {
   print("Olá me chamo Dart, qual seu nome?");
-  var entrada = stdin.readLineSync();;
-  print("Muito prazer, $entrada");
+  var nome = stdin.readLineSync();
+  
+  print("Qual sua idade?");
+  var idade = stdin.readLineSync();
+
+  print("Muito prazer, $nome, você tem $idade anos!");
 }
